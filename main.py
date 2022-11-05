a03bf15d95c7cb92bc3d73a943155f0f3a3ade37
koxan <koxanovskij86@mail.ru>
2022-11-06 01:25:13 +0300

    Дисперсия и стандартное отклонение

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from statistics import mean
 from typing import List
+from scipy import linalg
+import math
 
 # ВАРИАЦИЯ -  мера разброса данных
 
 # 1) размах - разница между максимальными и минимальными размерами данных
-data_list: List[int] = [1, 2, 3, 5, 8, 23, 59, 123, 900]
+data_list: List[int] = [1, 2, 3, 5, 8, 23, 59, 23, 700]
 
 
 def data_range(x: List[int]) -> int:
@@ -28,3 +30,18 @@ def de_mean(xs: List[float]) -> List[float]:
 print(de_mean(data_list))
 
 print('Получилось нулевое среднее - ', mean(de_mean(data_list)))
+
+
+#
+# Расчет самой дисперсии
+def variance(xs: List[float]) -> float:
+    assert len(xs) >= 2, 'Дисперсия требует не менее двух элементов'
+    n = len(xs)
+    deviation = de_mean(xs)
+    sum_of_squares = sum([x ** 2 for x in deviation]) / (n - 1)
+    return sum_of_squares
+
+
+print(variance(data_list))
+# корень из дисперсии - стандартное отклонение
+print(math.sqrt(variance(data_list)))
